866a8359439c3d58814887374f863e58227e287c
Amine bellaala <amin14c@gmail.com>
2026-05-27 12:47:40 +0100

    Update MuzikApp.kt

--- app/src/main/java/com/amindev/muziktube/MuzikApp.kt
+++ app/src/main/java/com/amindev/muziktube/MuzikApp.kt
@@ -2,10 +2,16 @@ package com.amindev.muziktube
 
 import android.app.Application
 import org.schabi.newpipe.extractor.NewPipe
+import org.schabi.newpipe.extractor.localization.ContentCountry
+import org.schabi.newpipe.extractor.localization.Localization
 
 class MuzikApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        NewPipe.init(YtDownloader.getInstance())
+        NewPipe.init(
+            YtDownloader.getInstance(),
+            Localization("en", "US"),
+            ContentCountry("US")
+        )
     }
 }
